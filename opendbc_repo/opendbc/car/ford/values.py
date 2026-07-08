fbd5ff3147b681ff69985f380368ec3e5f6cf74f
Kacer Aleks <kacper4553@gmail.com>
2026-07-08 15:17:08 +0200

    Add fingerprint for custom mondeo mk4

--- opendbc_repo/opendbc/car/ford/values.py
+++ opendbc_repo/opendbc/car/ford/values.py
@@ -175,6 +175,10 @@ class CAR(Platforms):
     [FordCarDocs("Ford Focus 2018-22", "Adaptive Cruise Control with Lane Centering", footnotes=[Footnote.FOCUS], hybrid=True)],  # mHEV only
     CarSpecs(mass=1350, wheelbase=2.7, steerRatio=15.0),
   )
+  FORD_MONDEO_MK4 = FordCANFDPlatformConfig(
+    [FordCarDocs("Ford Mondeo 2007-2014", "Adaptive Cruise Control with Lane Keeping Aid (if equipped)")],
+    CarSpecs(mass=1550, wheelbase=2.85, steerRatio=16.0),
+  )
   FORD_MAVERICK_MK1 = FordPlatformConfig(
     [
       FordCarDocs("Ford Maverick 2022", "LARIAT Luxury", hybrid=True),
